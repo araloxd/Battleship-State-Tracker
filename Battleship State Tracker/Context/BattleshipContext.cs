bcd21d5b1e28403ec473008d2fc9790e6d1bbdba
DESKTOP-P77KUMS\Edgy <gonzaloarzubiaga@gmail.com>
2022-03-27 18:21:09 -0300

    Added db Contexts.

---
+++ Battleship State Tracker/Context/BattleshipContext.cs
@@ -0,0 +1,20 @@
+﻿using Battleship_State_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Battleship_State_Tracker.Context
+{
+    public class BattleshipContext: DbContext
+    {
+        public DbSet<Room>? Rooms { get; set; }
+        public DbSet<RoomStatus>? RoomStatus { get; set; }
+        public DbSet<Player>? Players { get; set; }
+        public DbSet<Board>? Boards { get; set; }
+        public DbSet<Boat>? Boats { get; set; }
+        public DbSet<BoatStatus>? BoatStatus { get; set; }
+        public DbSet<Position>? Positions { get; set; }
+        public DbSet<Shoot>? Shoots { get; set; }
+        public DbSet<Turn>? Turns { get; set; }
+
+        public BattleshipContext(DbContextOptions<BattleshipContext> options) : base(options) { }
+    }
+}
