199ce0110f53dd237ba956c97e2546bcb91247c6
DESKTOP-P77KUMS\Edgy <gonzaloarzubiaga@gmail.com>
2022-03-30 09:52:16 -0300

    Amazon deps, swagger for all envs.

--- Battleship State Tracker/Program.cs
+++ Battleship State Tracker/Program.cs
@@ -38,11 +38,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
