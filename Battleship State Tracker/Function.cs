af30fa06ec66b8641fc85307b9ef830b50b08250
DESKTOP-P77KUMS\Edgy <gonzaloarzubiaga@gmail.com>
2022-03-30 17:21:04 -0300

    Updated FunctionHandler to test.

--- Battleship State Tracker/Function.cs
+++ Battleship State Tracker/Function.cs
@@ -1,4 +1,6 @@
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,15 +9,22 @@ namespace AWSLambda1;
 
 public class Function
 {
-    
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public string FunctionHandler(FunctionInput input, ILambdaContext context)
     {
-        return input.ToUpper();
+        return JsonConvert.SerializeObject(input).ToString();
     }
+}
+
+public class FunctionInput
+{
+    public string Key1 { get; set; }
+    public string Key2 { get; set; }
+    public string Key3 { get; set; }
 }
